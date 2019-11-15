3d446054f5664270e0e075081d0ec66d1b455851
Peter Gundel <mail@petergundel.de>
2019-11-15 14:48:20 +0100

    ✨ Create get-issue-ref action

---
+++ get-issue-ref/src/index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function run() {
+  try {
+    const eventName = core.getInput('event_name')
+    const event = JSON.parse(core.getInput('event'))
+    const githubToken = core.getInput('githubToken')
+
+    const octokit = new github.GitHub(githubToken)
+
+    const url = event.issue.pull_request.url
+    const urlParts = url.split('/')
+    const owner = urlParts[4]
+    const repo = urlParts[5]
+    const pull_number = urlParts[7]
+
+    //  https://developer.github.com/v3/pulls/#get-a-single-pull-request
+    const { data: pullRequest } = await octokit.pulls.get({
+      owner,
+      repo,
+      pull_number,
+    })
+
+    const ref = pullRequest.head.ref
+    const sha = pullRequest.head.sha
+    core.setOutput('ref', ref)
+    core.setOutput('sha', sha)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
